892e01b90e0763591eb3164ffa6bc5221b2ca5ee
Rondinelli Augusto Franca <rondinelli.franca@bs2.com>
2019-05-03 17:15:00 -0300

    Add equipamento viewmodel

--- Pilar_Facilitis_Sistem/Pilar_Facilitis.Domain/ViewModel/EquipamentoViewModel.cs
+++ Pilar_Facilitis_Sistem/Pilar_Facilitis.Domain/ViewModel/EquipamentoViewModel.cs
@@ -1,7 +1,17 @@
-﻿namespace Pilar_Facilitis.Domain.ViewModel
+﻿using System;
+
+namespace Pilar_Facilitis.Domain.ViewModel
 {
     public class EquipamentoViewModel
     {
-        
+        public Guid Id { get; set; }
+
+        public string Desc_Equip { get; set; }
+
+        public float Capacidade { get; set; }
+
+        public string Fabricante { get; set; }
+
+        public string NumDeSerie { get; set; }
     }
 }
